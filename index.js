14a6bcb0d3c8231620a20a7659a134f977271517
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-17 00:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-  user.prototype.veto = function(){
+  user.prototype.veto = function() {
     return 'No, I must disagree'
   }
 
-  user.prototype.approve = function(){
+  user.prototype.approve = function() {
     return 'You can do that!'
   }
 
-  user.prototype.doCharity = function(){
+  user.prototype.doCharity = function() {
     return "I like to help people."
   }
 
-  user.prototype.releasePressStatement = function(){
+  user.prototype.releasePressStatement = function() {
     return "You will see great things from Scuber."
   }
 
-  user.prototype.sayHi = function(){
+  user.prototype.sayHi = function() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-
